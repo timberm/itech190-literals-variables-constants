d6c37f3bbcd6719738806ae4811722cd848dfe7a
Timber Michaud <timichaud@stu.lltc.edu>
2024-09-12 10:34:29 -0500

    second commit

--- index.py
+++ index.py
@@ -1,5 +1,27 @@
 def main():
     print("Hello World")
+    #Create 4 variables using the correct casing
+    #constant called pi
+    #string called my name
+    #num called telephone num
+    #string called telephone num
+
+    PI = 3.14
+    my_name = "Timber"
+    telephone_num = 218555555
+    telephone_num_str = "218555555"
+    print()
+    print(PI)
+    print("Pi is of type ", type(PI))
+    print()
+    print(my_name)
+    print("my_name is of type ", type(my_name))
+    print()
+    print(telephone_num)
+    print("telephone_num is of type ", type(telephone_num))
+    print()
+    print(telephone_num_str)
+    print("telephone_num_str is of type ", type(telephone_num_str))
 
 if __name__ == "__main__":
     main()
